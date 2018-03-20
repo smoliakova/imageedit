0130c784a6056ba92bf90663a9acb63fecdf16fc
Катя Смолякова <blackhepburn@gmail.com>
2018-03-21 00:28:23 +0300

    rotate function

--- ImageEdit/ViewController.swift
+++ ImageEdit/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func rotateImage(_ sender: Any) {
+        UIView.animate(withDuration: 0.5) {
+            let angel = Double((sender as AnyObject).titleForSegment(at: (sender as AnyObject).selectedSegmentIndex)!)!
+            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(angel/180 * .pi))
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
