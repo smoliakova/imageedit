780ffe9af6b3f6844bc81e9b2e3c7ff9df22ff14
Катя Смолякова <blackhepburn@gmail.com>
2018-03-22 14:13:26 +0300

    sizestepper

--- ImageEdit/ViewController.swift
+++ ImageEdit/ViewController.swift
@@ -9,27 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var numberOfSize: UILabel!
+    
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBAction func rotateImage(_ sender: Any) {
-        UIView.animate(withDuration: 0.5) {
-            let angel = Double((sender as AnyObject).titleForSegment(at: (sender as AnyObject).selectedSegmentIndex)!)!
-            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(angel/180 * .pi))
-        }
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    
+    @IBAction func rotateImage(_ sender: Any) {
+        UIView.animate(withDuration: 0.5) {
+            let angle = Double((sender as AnyObject).titleForSegment(at: (sender as AnyObject).selectedSegmentIndex)!)!
+            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat(angle/180 * .pi))
+        }
+    }
+    
+    @IBAction func sizeStepper(_ sender: UIStepper) {
+        let stepperValue = Int(sender.value)
+        numberOfSize.text = String(stepperValue)
+        
+        
+        
+  // self.numberOfSize.text = sender.value.description
+        
+        
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
 
